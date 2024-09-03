a171891eb9d3080301c8710a0fe0565a5c87523d
mrdevsam <mdshoykotislam05@gmail.com>
2024-09-03 19:27:06 +0600

    initial commit

---
+++ src/test/java/com/example/springdatajpa_inheritance/SpringdatajpaInheritanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdatajpa_inheritance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdatajpaInheritanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
